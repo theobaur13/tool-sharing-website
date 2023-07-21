0bf723f8fe8a58ae44d132947043dd12fe407198
Theo Baur <baurt@cardiff.ac.uk>
2023-07-21 17:34:44 +0100

    fixed minor import issue

--- tool_sharing_website/models.py
+++ tool_sharing_website/models.py
@@ -1,5 +1,5 @@
 from tool_sharing_website import db, login_manager
-from datetime import datetime
+from datetime import datetime, timedelta
 from flask_login import UserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
 
